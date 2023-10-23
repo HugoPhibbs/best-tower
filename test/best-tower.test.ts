7cd72622c8214a7b328682fec0948cfc607d97a7
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-10-23 18:11:27 +1300

    adding some basic tests for finding towers

--- test/best-tower.test.ts
+++ test/best-tower.test.ts
@@ -1,6 +1,35 @@
 const {getFarmTowers, findBestTower} = require("../src/best-tower")
 
+const nock = require("nock")
+
 describe("Test getting farm towers", () => {
+    test("Test normally", async () => {
+        nock("https://api.onizmx.com")
+            .get("/lambda/tower_stream")
+            .reply(200, [
+                "https://api/test_data_1.csv",
+                "https://api/test_data_2.csv",
+                "https://api/test_data_3.csv"
+            ], {
+                "Content-Type": "application/json"
+            })
+
+        nock("https://api").get("/test_data_1.csv").replyWithFile(200, __dirname + '/data/test_data_1.csv')
+        nock("https://api").get("/test_data_2.csv").replyWithFile(200, __dirname + '/data/test_data_2.csv')
+        nock("https://api").get("/test_data_3.csv").replyWithFile(200, __dirname + '/data/test_data_3.csv')
+
+        const result = await getFarmTowers("f1")
+
+        expect(result).toStrictEqual([
+            { towerId: 't1', rssi: '10' },
+            { towerId: 't1', rssi: '14' },
+            { towerId: 't1', rssi: '16' },
+            { towerId: 't6', rssi: '20' },
+            { towerId: 't6', rssi: '30' },
+            { towerId: 't1', rssi: '4' },
+            { towerId: 't1', rssi: '6' }
+        ])
+    })
 
 })
 
@@ -22,6 +51,9 @@ describe("Test finding the best tower", () => {
         expect(result.averageRssi).toBe(6)
     })
 
-    describe("Test with empty input", () => {
+    test("Test with empty input", () => {
+        const result = findBestTower([])
+        expect(result.towerId).toBe(undefined)
+        expect(result.averageRssi).toBe(Number.NEGATIVE_INFINITY)
     })
 })
