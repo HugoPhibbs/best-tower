50037ff96e817ab6ae1c0d18d4bec6bffea45b44
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-10-23 18:12:31 +1300

    Removing logging of done in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 const {main: bestTower} = require("./best-tower")
 
-bestTower().then(() => console.log("Done!"))
+// I've added an individual file here, so the main program can be tested easier
+
+// Start the program, don't bother handling the promise
+bestTower()
